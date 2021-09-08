d45fc1ce2ac6fbba2e279ecd9a38d7d58f444d16
Thais Campanac-Climent <thais.campanac@gmail.com>
2021-09-08 12:10:04 -0500

    Welcome message

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,14 @@ export default function Home() {
           </nav>
       </div>
       <div className="content">
+        <div className="new-website">
+          <h1>Welcome</h1>
+          <h2>New website, new feelings</h2>
+          <p>The last website that I made was hard-coded with HTML and CSS.</p>
+          <p>I know now that I need a refresh and a better way to present myself and my projects.</p>
+          <p>So goodbye with the old and hello to Website Upgrade.</p>
+          <h2>Future Updates to Come!</h2>
+        </div>
         <div className="welcome">
           <h1>Hi There!</h1>
           <h2>A Little About Me</h2>
