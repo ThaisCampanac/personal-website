2fa81cf6938b69980a0a896c3246ecf4f8b4ac46
ThaisCampanac <thais.campanac@gmail.com>
2022-05-27 11:58:40 -0500

    Complete data items for timeline

--- pages/data.js
+++ pages/data.js
@@ -3,43 +3,49 @@ const items = [
         title: "June - August 2019",
         cardTitle: "Google CSSI",
         cardSubtitle: "Coursera Student",
-        cardDetailedText:"Link to Certificate"
+        cardDetailedText:"The course reviewed the applications of Java and the Google Maps library."
     },
     {
         title: "August 2019",
         cardTitle: "University of Texas at Dallas",
         cardSubtitle: "Software Engineer Major",
-        cardDetailedText:""
-    },
-    {
-        title: "Feburary 2021",
-        cardTitle: "Personal Project",
-        cardSubtitle: "Genome Circular",
-        cardDetailedText:"Link to projects page here"
-    },
-    {
-        title: "Fall 2020",
-        cardTitle: "Academic Project",
-        cardSubtitle: "Sorting Algorithm",
-        cardDetailedText:"Link to projects page here"
+        cardDetailedText:"I began my first semester at UTD."
     },
     {
         title: "Fall 2020",
         cardTitle: "Actvities",
         cardSubtitle: "EPICS Student and WebMaster",
-        cardDetailedText:"Link to projects page here"
+        cardDetailedText:"Within EPICS, I was assigned to the FACE project. The FACE project required a website that allowed FACE to go through their full lifecycle with a report."
     },
     {
         title: "Spring 2021",
         cardTitle: "University of Texas at Dallas",
         cardSubtitle: "Computer Science Major",
-        cardDetailedText:"Link to projects page here"
+        cardDetailedText:"I decided to do another major."
+    },
+    {
+        title: "May - November 2021",
+        cardTitle: "ACM at UTD",
+        cardSubtitle: "HackUTD Web Developer",
+        cardDetailedText:"Throughout this time I was the lead frontend developer for the Hackathon website."
     },
     {
         title:"June - July 2021",
         cardTitle:"Banana Republic Sales Associate",
         cardSubtitle:"Stylist",
-        cardDetailedText:"Worked with people"
+        cardDetailedText:"I have worked with clients to help them with finding products, trying products, and checking out."
+    },
+    {
+        title:"December - January 2021-22",
+        cardTitle:"Banana Republic Sales Associate",
+        cardSubtitle:"Stylist",
+        cardDetailedText:"I have worked with clients to help them with finding products, trying products, and checking out."
+    },
+    {
+        title:"June - July 2022",
+        cardTitle:"PriceWaterHouse and Copper",
+        cardSubtitle:"Security Developer Intern",
+        cardDetailedText:"TBD"
     }
 ];
 
